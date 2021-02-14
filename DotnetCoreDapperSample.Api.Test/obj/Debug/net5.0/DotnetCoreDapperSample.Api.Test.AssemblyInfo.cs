5c6358aa2fc2101e9c112687c2927025da125197
whitedovey <whitedovey@gmail.com>
2021-02-14 14:22:52 +0900

    테스트 폴더 삭제

--- DotnetCoreDapperSample.Api.Test/obj/Debug/net5.0/DotnetCoreDapperSample.Api.Test.AssemblyInfo.cs
+++ DotnetCoreDapperSample.Api.Test/obj/Debug/net5.0/DotnetCoreDapperSample.Api.Test.AssemblyInfo.cs
@@ -11,6 +11,8 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.AspNetCore.Mvc.Testing.WebApplicationFactoryContentRootAttribute("DotnetCoreDapperSample.Api, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null" +
+    "", "D:\\GitHubTemp\\dotnetcore\\DotnetCoreDapperSample.Api", "DotnetCoreDapperSample.Api.csproj", "0")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotnetCoreDapperSample.Api.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
